1e4d12a1af0a92d9fd54a14f0e120218c38dc5b6
JohnnyDionisio <pedro.souza99@portalsesisp.org.br>
2026-06-11 15:54:12 -0300

    Adicionando o Projeto ao Github

---
+++ ProjetoFinal/Controllers/InstrumentosController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetoFinal.Controllers
+{
+    public class InstrumentosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Authorize(Roles = "Admin")]
+        public IActionResult AdicionarInstrumentos()
+        {
+            return View();
+        }
+        public IActionResult Orgao()
+        {
+            return View();
+        }
+        public IActionResult Madeiras()
+        {
+            return View();
+        }
+        public IActionResult Metais()
+        {
+            return View();
+        }
+        public IActionResult Cordas()
+        {
+            return View();
+        }
+        public IActionResult Teclas()
+        {
+            return View();
+        }
+        public IActionResult Percussao()
+        {
+            return View();
+        }
+
+    }
+}
