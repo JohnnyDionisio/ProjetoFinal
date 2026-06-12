110492f8c9ccec77631c21139ae39b0f92db2942
JohnnyDionisio <pedro.souza99@portalsesisp.org.br>
2026-06-12 07:36:08 -0300

    arrumando erros de digitação

--- ProjetoFinal/obj/Debug/net10.0/ProjetoFinal.AssemblyInfo.cs
+++ ProjetoFinal/obj/Debug/net10.0/ProjetoFinal.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoFinal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e4d12a1af0a92d9fd54a14f0e120218c38dc5b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoFinal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoFinal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
